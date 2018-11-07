a3b4fe80707fc4bd66c4636c1163ae927e2a7667
MaksymChumak <chumak.maksym@gmail.com>
2018-11-06 19:59:49 -0800

    create styles for login page

--- server.js
+++ server.js
@@ -6,13 +6,27 @@ const port = process.env.PORT || 8080;
 let app = express();
 
 app.set('views', `${__dirname}/views`);
+
 app.set('view engine', 'hbs');
+app.use(express.static(__dirname + "/public"));
 
 app.get('/', (request, response) => {
   response.render('index.hbs');
-})
+});
+
+app.get('/game', (request, response) => {
+  response.render('game.hbs');
+});
+
+app.get('/account', (request, response) => {
+  response.render('account.hbs');
+});
+
+app.get('/leaderboard', (request, response) => {
+  response.render('leaderboard.hbs');
+});
 
 
 app.listen(port, () => {
   console.log(`Server is up on port 8080`);
-})
+});
